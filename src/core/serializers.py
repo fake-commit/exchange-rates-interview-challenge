c727e611f3f17873cee8ff71b80e7a368794a66d
Timur Bacheev <timur_bacheev@epam.com>
2021-11-01 23:04:48 +0300

    add id field to API /api/v1/quotes/ response

--- src/core/serializers.py
+++ src/core/serializers.py
@@ -7,7 +7,7 @@ class ExchangeRateSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = ExchangeRate
-        fields = ('from_currency', 'to_currency', 'rate', 'timestamp')
+        fields = ('id', 'from_currency', 'to_currency', 'rate', 'timestamp')
         extra_kwargs = {
             'from_currency': {'required': True},
             'to_currency': {'required': True},
